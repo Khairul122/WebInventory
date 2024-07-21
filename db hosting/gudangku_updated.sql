aa9ecc03f0282dc214d649ccc1f6a85bc053b69f
Khairul122 <khairulhuda242@gmail.com>
2024-07-21 16:05:44 +0700

    fix bug hosting

--- db hosting/gudangku_updated.sql
+++ db hosting/gudangku_updated.sql
@@ -2,10 +2,10 @@
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jul 17, 2024 at 01:19 AM
--- Server version: 10.4.25-MariaDB
--- PHP Version: 7.4.30
+-- Host: localhost:3306
+-- Generation Time: Jul 21, 2024 at 08:57 AM
+-- Server version: 8.0.30
+-- PHP Version: 7.4.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,21 +28,46 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `barang` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `namaBarang` varchar(32) NOT NULL,
-  `stok` int(11) NOT NULL,
+  `stok` int NOT NULL,
+  `nama_mesin1` varchar(100) NOT NULL,
+  `jumlah1` int NOT NULL,
+  `nama_mesin2` varchar(100) NOT NULL,
+  `jumlah2` int NOT NULL,
+  `nama_mesin3` varchar(100) NOT NULL,
+  `jumlah3` int NOT NULL,
   `hapus` varchar(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `barang`
 --
 
-INSERT INTO `barang` (`id`, `namaBarang`, `stok`, `hapus`) VALUES
-(1, 'Roma malkist', 15, '1'),
-(2, 'Indomie Soto', 0, '1'),
-(3, 'Kopiko', 2, '1'),
-(4, 'Buku', 7, '0');
+INSERT INTO `barang` (`id`, `namaBarang`, `stok`, `nama_mesin1`, `jumlah1`, `nama_mesin2`, `jumlah2`, `nama_mesin3`, `jumlah3`, `hapus`) VALUES
+(1, 'Roma malkist', 15, '0', 0, '0', 0, '0', 0, '1'),
+(2, 'Indomie Soto', 0, '0', 0, '0', 0, '0', 0, '1'),
+(3, 'Kopiko', 2, '0', 0, '0', 0, '0', 0, '1'),
+(4, 'Buku', 7, '0', 0, '0', 0, '0', 0, '1'),
+(5, 'Kopi', 302, 'Mesin 1', 101, 'Mesin 1', 101, 'Mesin 1', 101, '0');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mesin`
+--
+
+CREATE TABLE `mesin` (
+  `id_mesin` int NOT NULL,
+  `nama_mesin` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `mesin`
+--
+
+INSERT INTO `mesin` (`id_mesin`, `nama_mesin`) VALUES
+(2, 'Mesin 1');
 
 -- --------------------------------------------------------
 
@@ -51,19 +76,19 @@ INSERT INTO `barang` (`id`, `namaBarang`, `stok`, `hapus`) VALUES
 --
 
 CREATE TABLE `transaksi` (
-  `id_transaksi` int(11) NOT NULL,
+  `id_transaksi` int NOT NULL,
   `tgl_transaksi` date NOT NULL,
   `nama_costumer` varchar(100) NOT NULL,
   `tujuan` varchar(255) NOT NULL,
-  `qty` int(11) NOT NULL,
+  `qty` int NOT NULL,
   `no_mobil` varchar(15) NOT NULL,
   `nama_supir` varchar(100) NOT NULL,
   `no_hp` varchar(20) NOT NULL,
   `metode_bayar` enum('cash','credit') NOT NULL,
   `shift` enum('pagi','malam') NOT NULL,
   `status` enum('pengajuan','antrian','jalan','batal','acc') NOT NULL,
-  `id_barang` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `id_barang` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `transaksi`
@@ -71,8 +96,8 @@ CREATE TABLE `transaksi` (
 
 INSERT INTO `transaksi` (`id_transaksi`, `tgl_transaksi`, `nama_costumer`, `tujuan`, `qty`, `no_mobil`, `nama_supir`, `no_hp`, `metode_bayar`, `shift`, `status`, `id_barang`) VALUES
 (1, '2024-07-17', 'Budi', 'Bekasi', 1, 'HIU1', 'Budi1', '082165443677', 'cash', 'pagi', 'acc', 4),
-(2, '2024-07-16', 'Budi', 'Bekasi', 2, '12', 'Budi', '082165443677', 'cash', 'pagi', 'acc', 4),
-(3, '2024-07-17', 'ANDI', 'HUIU', 1, '2', 'Andika', '082165443677', 'credit', 'malam', 'acc', 4);
+(4, '2024-07-21', 'Andika', 'Bekasi', 1, 'HIU1', 'Budi1', '082165443677', 'cash', 'pagi', 'jalan', 5),
+(5, '2024-07-21', 'Andi', 'Bekaso', 3, 'HIU1', 'Budi1', '082165443677', 'cash', 'pagi', 'pengajuan', 5);
 
 -- --------------------------------------------------------
 
@@ -81,11 +106,11 @@ INSERT INTO `transaksi` (`id_transaksi`, `tgl_transaksi`, `nama_costumer`, `tuju
 --
 
 CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `nama` varchar(32) NOT NULL,
   `password` varchar(64) NOT NULL,
   `rule` varchar(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `user`
@@ -104,6 +129,12 @@ INSERT INTO `user` (`id`, `nama`, `password`, `rule`) VALUES
 --
 ALTER TABLE `barang`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `mesin`
+--
+ALTER TABLE `mesin`
+  ADD PRIMARY KEY (`id_mesin`);
 
 --
 -- Indexes for table `transaksi`
@@ -126,19 +157,25 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `barang`
 --
 ALTER TABLE `barang`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `mesin`
+--
+ALTER TABLE `mesin`
+  MODIFY `id_mesin` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `transaksi`
 --
 ALTER TABLE `transaksi`
-  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_transaksi` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
